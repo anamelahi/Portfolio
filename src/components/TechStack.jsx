745fcb88b128af5835f774da70bd7a2fccc00305
Anam Elahi <anamelahi04@gmail.com>
2024-06-26 21:19:36 +0530

    Updated techstack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,9 +3,10 @@ import html from "../html.png"
 import css from "../css-3.png"
 import js from "../js.png"
 import python from "../python.png"
-import figma from "../figma.png"
 import reactlogo from "../react-logo.png"
 import nodejs from "../nodejs.png"
+import mongodb from "../mongo.png"
+import express from "../express.png"
 
 function TechStack(){
     return(
@@ -16,8 +17,11 @@ function TechStack(){
             <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={js} alt="" />
             <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={reactlogo} alt="" />
             <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={nodejs} alt="" />
+            <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={express} alt="" />
+
+            <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={mongodb} alt="" />
             <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={python} alt="" />
-            <img style={{width:"30px",height:"30px",margin:"10px 5px 5px 10px"}} src={figma} alt="" />
+
 
 
         </div>
